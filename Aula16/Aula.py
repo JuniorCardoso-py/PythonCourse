bb5c9a3f77b169191c46440bc67f9e516648e0c9
ElementaryBR <juniorcardoso.sc@outlook.com>
2019-11-29 11:27:50 -0300

    Aula 16

--- Aula16/Aula.py
+++ Aula16/Aula.py
@@ -3,13 +3,16 @@
 #Crie um metodo
 #cadastro de playlist
 #lendo musica, artista e album
-from teste import criar_faixa
+from teste import criar_faixa, salvar_faixa, ler_faixa #Chamar duas funções separar por virgulas.
 
-musica = input('Digite o nome da musica: ')
-banda = input('Digite o nome da banda: ')
-album = input('Digite o nome do album da musica: ')
-#dados_banda = [musica,banda,album]
-#faixa = {'musica':musica, 'artista':banda, 'album':album}
-faixa = criar_faixa(musica,banda,album)
+# musica = input('Digite o nome da musica: ') # adicionando dado para colocar dentro de um metodo que vai receber a variavel para salvar dentro de um arquivo texo
+# banda = input('Digite o nome da banda: ') # adicionando dado para colocar dentro de um metodo que vai receber a variavel para salvar dentro de um arquivo texo
+# album = input('Digite o nome do album da musica: ') # adicionando dado para colocar dentro de um metodo que vai receber a variavel para salvar dentro de um arquivo texo
+# #dados_banda = [musica,banda,album] # Aqui fiza só para ver como fica
+# #faixa = {'musica':musica, 'artista':banda, 'album':album} # Aqui criei um dicionario para depois poder chamar na função pela chave de cada variavel adicionada
+# faixa = criar_faixa(musica,banda,album) # Aqui criei uma variavel que vai receber o metodo de criar uma faixa que vai receber o dicionario criado acima
+# save = salvar_faixa(faixa) # aqui criei uma variavel que vai receber o metodo que vai receber o que o metodo que vai salvar dentro do arquivo texto, oque será escrito 
+lista = ler_faixa() # pode salvar dentro de uma variavel ou dar um print # Aqui vai receber o metodo para leitura doq esta dentro do arquivo texto para leitura dentro do console
 
-open('faixas.txt','a')
+for faixa in lista:
+    print(f"{faixa['musica']} - {faixa['artista']} - {faixa['album']}")
--- Aula16/teste.py
+++ Aula16/teste.py
@@ -1,3 +1,22 @@
 def criar_faixa(musica, banda, album):
     faixa = {'musica':musica, 'artista':banda, 'album':album}
     return faixa
+
+# funções interligadas
+
+def salvar_faixa(faixa):
+    arquivo = open('Aula16/faixas.txt','a')
+    arquivo.write(f"{faixa['musica']};{faixa['artista']};{faixa['album']}\n")
+    arquivo.close()
+    return faixa
+
+def ler_faixa():
+    arquivo = open('Aula16/faixas.txt','r')
+    lista_faixas = []
+    for linha in arquivo: # Aqui ele vai ler todos o elementos dentro da variavel arquivo
+        linha = linha.strip() # limpo linha
+        dados_faixa = linha.split(';') # condição de separação das strings
+        faixa = criar_faixa(dados_faixa[0],dados_faixa[1],dados_faixa[2])
+        lista_faixas.append(faixa) # adicionar todos os dados da variavel faixa para uma lista
+    arquivo.close() # sempre fechar usando esse metodo.
+    return lista_faixas
