30f9f6b01f35f3a34540844fa1866a09b7866c2c
Junior Cardoso <50811299+ElementaryBR@users.noreply.github.com>
2020-01-08 11:40:55 -0300

    Aula31

---
+++ Aula31/Aula31.py
@@ -0,0 +1,40 @@
+#Banco de Dados                                                             #### Relacional
+# ORN
+# SGBD - Sistema Gerenciador de Banco De Dados
+# MySQL, SQLServer ....
+
+#MySQL = MariaDb
+#select * from Pessoa where id = 1      Filtrar pelo indice
+#select * from Pessoa where id > 1      Filtrar pelo indice(MAIOR QUE 1)
+#select * from Pessoa where id < 2     Filtrar pelo indice(MENOR QUE 2)
+#select * from Pessoa where id <> 1      Filtrar por diferente de ( No Caso 1)
+
+#Insert Into Pessoa
+# (
+#   Nome
+#   ,Sobrenome
+#   ,Idade
+# 
+# )     
+# Values
+# ( 
+#   'TAVIN'
+#   ,'HONDA'
+#   ,15
+# )
+#
+#
+# CRUD
+# C = CREAT (INSERIDO, SALVANDO)
+# R = READ (LENDO)
+# U = UPDATE (ALTERANDO)
+# D = DELETE (APAGANDO)
+#
+# update pessoa
+#     set idade = 24
+#     where id = 4
+
+
+
+
+
